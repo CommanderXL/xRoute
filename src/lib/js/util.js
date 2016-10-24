d5af114d7e74d63c1b5d8aa74d3d17bd3c07c27b
Xiaolei <xiaolei@didichuxing.com>
2016-10-24 11:24:16 +0800

    添加timeSelect-component

--- src/lib/js/util.js
+++ src/lib/js/util.js
@@ -53,6 +53,21 @@ class Util {
     isObj(obj) {
         return Object.prototype.toString.call(obj) === '[object Object]';
     }
+    //对模块进行配置
+    setConfigModule(arg_map) {
+        var input_map = arg_map.input_map,
+                settable_map = arg_map.settable_map,
+                config_map = arg_map.config_map;
+            for(var key in input_map) {
+                if(input_map.hasOwnProperty(key)) {
+                    if(settable_map.hasOwnProperty(key)) {
+                        config_map[key] = input_map[key];
+                    }
+                } else {
+                    console.error('Require a key name');
+                }
+            }
+    }
 }
 let util = new Util();
 
