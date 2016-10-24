00a9274d4a87b8f0e4fb4cfa887be89ba8879373
Xiaolei <xiaolei@didichuxing.com>
2016-10-24 09:42:18 +0800

    add uipicker.component

--- src/modules/pageA/a-controller.js
+++ src/modules/pageA/a-controller.js
@@ -1,6 +1,7 @@
 import {Controller} from '../../lib/js/controller';
 import modelA from './a-model';
 import {util, elementSet} from '../../lib/js/util';
+import Picker from '../../lib/js/picker.min';   //事件选择组件
 
 let controller = modelA.registerController('.page-container:first-child');
 
@@ -16,12 +17,133 @@ controller
         action() {
             elementSet.css(this, {color: 'red'});
         }
+    },
+    myName: {
+        actionName: 'input',
+        action(e) {
+            console.log(this.value);
+        }
+    },
+    myPhone: {
+        actionName: 'input',
+        action(e) {
+            console.log(this.value);
+        }
     }
 })
 //页面状态初始化.从localstorage中获取
 .getViewInit(function() {
     let doms = this.domMap;
     doms.myName.value = modelA.submitData.name;
+
+
+    var nameEl = document.getElementById('name');
+
+var data1 = [
+	{
+		text: '小美',
+		value: 1
+	}, {
+		text: '猪猪',
+		value: 2
+	}
+];
+
+var data2 = [
+	{
+		text: '张三',
+		value: 1
+	},
+	{
+		text: '李四',
+		value: 2
+	},
+	{
+		text: '王五',
+		value: 3
+	},
+	{
+		text: '赵六',
+		value: 4
+	},
+	{
+		text: '吴七',
+		value: 5
+	},
+	{
+		text: '陈八',
+		value: 6
+	},
+	{
+		text: '杜九',
+		value: 7
+	},
+	{
+		text: '黄十',
+		value: 8
+	},
+	{
+		text: '呵呵',
+		value: 9
+	},
+	{
+		text: '哈哈',
+		value: 10
+	},
+	{
+		text: '嘿嘿',
+		value: 11
+	},
+	{
+		text: '啦啦',
+		value: 12
+	}
+];
+
+var data3 = [
+	{
+		text: '开心',
+		value: 1
+	}, {
+		text: '生气',
+		value: 2
+	},
+	{
+		text: '搞笑',
+		value: 3
+	}, {
+		text: '难过',
+		value: 4
+	}
+];
+
+var picker = new Picker({
+	data: [data1, data2, data3],
+	selectedIndex: [0, 1, 2],
+	title: '我们都是小学生'
+});
+
+picker.on('picker.select', function (selectedVal, selectedIndex) {
+	nameEl.innerText = data1[selectedIndex[0]].text + ' ' + data2[selectedIndex[1]].text + ' ' + data3[selectedIndex[2]].text;
+})
+
+picker.on('picker.change', function (index, selectedIndex) {
+	console.log(index);
+	console.log(selectedIndex);
+});
+
+picker.on('picker.valuechange', function (selectedVal, selectedIndex) {
+	console.log(selectedVal);
+	console.log(selectedIndex);
+});
+
+nameEl.addEventListener('click', function () {
+	picker.show();
+});
+
+
+
+
 });
 
 export {
